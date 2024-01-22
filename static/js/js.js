7eb57437f62ddb420efaf8eea7f9d4a24077dec7
Jande12u <155047293+Jande12u@users.noreply.github.com>
2024-01-22 04:50:05 +0000

    Revert "main"
    This reverts commit 1b9477b5694ad45d2c0cfc591210e9543bdfa246.

--- static/js/js.js
+++ static/js/js.js
@@ -120,11 +120,12 @@ async function getIcon(id) {
 	if (urlIco !== null) {
 	  if (urlIco.href.includes('data:image/png;base64')) {
 		console.log("URL:", urlIco.href);
+		console.log(urlco.href)
 		return urlIco.href;
 	  }
        
 	  const res = await bare.fetch(urlIco.href);
-	
+	  console.log(url)
 	  const obj = URL.createObjectURL([await res.blob()], {
 		type: res.headers.get('content-type') || 'image/x-icon',
 	  });
@@ -138,6 +139,7 @@ async function getIcon(id) {
 	  const res = await bare.fetch(
 		new URL('/favicon.ico', CONTENT_WINDOW(id).location)
 	  );
+	  console.log(URL)
 	  const obj = URL.createObjectURL([await res.blob()], {
 		type: res.headers.get('content-type') || 'image/x-icon',
 	  });
