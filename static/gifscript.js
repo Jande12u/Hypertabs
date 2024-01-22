76d0c0b4a792424c101ac6bcf6d798eb0a53c6bf
Jande12u <155047293+Jande12u@users.noreply.github.com>
2024-01-21 16:49:41 -0800

    Update gifscript.j

--- static/gifscript.js
+++ static/gifscript.js
@@ -9,9 +9,6 @@ const normalGifs = [
     "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWhmamlmdDN3amk3bGg2M3loaXZ2OGtjMHB2dDcwcnloMGR2and5dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/E0fzCBH4UfwvS/giphy.gif",
     "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXh6b20xdmNxdHJra2xid2YwYXZyanZ1ZTllOGM0NWs1bHcwejZscCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/14xP57Ybt4vidG/giphy.gif",
     "https://media.giphy.com/media/JVxMKcI1qUcRW/giphy.gif"
-];
-
-const hoverGifs = [
     "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3ViN2x6MWQ0bXJjdWI1MmgwNm5oaDdqN2V5MGhjc3I0ZjVpb284dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NyGba8cVIgScw/giphy.gif",
     "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaXBkOWp4b3FtcTVhdXJrcTBpbDk3dnczeHFvcDdhZng2emdobXY4dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ZmZ5hwzX1dgQw/giphy.gif",
     "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmhvbzRpbW92YzgzM2s0c29qMmczaDRvMHRsY2kwOGUza2E4bWs4aSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/CQw94V8AMa556/giphy.gif",
@@ -21,31 +18,11 @@ const hoverGifs = [
     "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbG4yd2tpZWllcWx0ZXZoYnN0ZmRnNzRtam83cnRkOXpzMGJlajNrMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/iLD0cclRovNFC/giphy.gif"
 ];
 
-let currentHoverGif = "";
+let normalInterval;
 
-function startHover() {
-    clearInterval(normalInterval); // Clear the interval for normal GIFs
-
-    // Choose a random hover gif
-    currentHoverGif = getRandomGif(hoverGifs);
-    document.querySelector('.hover').src = currentHoverGif;
-
-    // Add a class to the container when hovering
-    document.querySelector('.container').classList.add('hovering');
-}
-
-function stopHover() {
-    // Change back to a random normal gif
+function changeNormalGif() {
     const randomNormalGif = getRandomGif(normalGifs);
     document.querySelector('.normal').src = randomNormalGif;
-
-    // Start the interval to change the normal gif every 15 seconds
-    normalInterval = setInterval(() => {
-        document.querySelector('.normal').src = getRandomGif(normalGifs);
-    }, 15000);
-
-    // Remove the class added when not hovering
-    document.querySelector('.container').classList.remove('hovering');
 }
 
 function getRandomGif(gifArray) {
